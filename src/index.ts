4670a3e85891648a322bc11b48fbf200ade03e1a
Patrick Pulwey <patrick.pulwey@cloudbucket.de>
2024-01-31 13:21:23 +0100

    init

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import cors from "cors";
+import express from "express";
+
+import terms from "./suggestions.json";
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.get("/terms", (req, res) => {
+  res.send(terms);
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
