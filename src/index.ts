ce7590d6083e7b4827a912123f27fc7f28178700
Patrick Pulwey <patrick.pulwey@cloudbucket.de>
2024-02-28 17:16:28 +0100

    search file and page

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import cors from "cors";
-import express from "express";
+import express, { Request } from "express";
+import path from "path";
 
 import terms from "./suggestions.json";
 
@@ -11,6 +12,28 @@ app.use(cors());
 app.get("/terms", (req, res) => {
   res.send(terms);
 });
+
+app.get(
+  "/suche/mehr",
+  async (
+    req: Request<{}, {}, {}, { q: string; cp: number; mp: number }>,
+    res
+  ) => {
+    const q = req.query.q;
+    const cp = req.query.cp;
+    const mp = req.query.mp;
+
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+
+    if (cp !== undefined) {
+      return res.sendFile(path.join(__dirname, "search", "page.html"));
+    } else if (mp !== undefined) {
+      return res.sendFile(path.join(__dirname, "search", "file.html"));
+    }
+
+    return res.status(404).send("Not Found");
+  }
+);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
